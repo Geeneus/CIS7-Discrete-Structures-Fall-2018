e6dd763d96cb1a0f749ed4c753e46323233142e0
Geeneus <36689179+Geeneus@users.noreply.github.com>
2018-10-19 00:35:48 -0700

    Update Assignment 7.cpp

--- Assignment 7/Assignment 7.cpp
+++ Assignment 7/Assignment 7.cpp
@@ -5,68 +5,77 @@ using namespace std;
 
 class HanoiTowers {
 	public:
+		int steps = 0;
 		int totalDisks;
 		vector<int> tower1;
 		vector<int> tower2;
 		vector<int> tower3;
 
+		// Initialize using total disks
 		HanoiTowers(int totalDiskInit) {
 			totalDisks = totalDiskInit;
-			tower1.resize(totalDisks);
-			tower2.resize(totalDisks);
-			tower3.resize(totalDisks);
-
-			for (int i = 0; i < totalDisks; i++) {
-				tower1[i] = i + 1;
-			}
+			for (int i = 0; i < totalDisks; i++) { tower1.push_back(totalDisks - i); }
 		}
 
-		void printDisks() {
-			for (int i = 0; i < totalDisks; i++) {
-				cout << (tower1[i] == 0) ? "-" : to_string(tower1[i]);
-			} cout << "\n";
+		void printTower(vector<int> &thisTower) {
+			for (int i = 0; i < totalDisks; i++) { cout << (i < thisTower.size() ? to_string(thisTower[i]) : "-") << " "; }
+			cout << "\n";
+		}
 
-			for (int i = 0; i < totalDisks; i++) {
-				cout << (tower2[i] == 0) ? "-" : to_string(tower2[i]);
-			} cout << "\n";
-
-			for (int i = 0; i < totalDisks; i++) {
-				cout << (tower3[i] == 0) ? "-" : to_string(tower3[i]);
-			} cout << "\n";
-
-			cout << "-- -- -- -- -- -- -- -- -- -- -- --" << "\n";
+		void printAllTowers() {
+			printTower(tower1);
+			printTower(tower2);
+			printTower(tower3);
+			cout << "\n";
 		}
 
 		void moveDisks(int thisDisk, vector<int> &start, vector<int> &goal, vector<int> &extra) {
-			if (thisDisk == 0) {
+			steps++;
+			if (thisDisk == 1) {
 				// Move final disk to goal 
-				start[thisDisk] = 0;
-				goal[thisDisk] = thisDisk + 1;
-				printDisks();
+				goal.push_back(start.back());
+				start.pop_back();
+				printAllTowers();
 			} else {
 				// Move all but bottom disk to extra
 				moveDisks(thisDisk - 1, start, extra, goal);
-
-				// After all disks are moved to extra, move bottom to goal
-				start[thisDisk] = 0;
-				goal[thisDisk] = thisDisk + 1;
-				printDisks();
-
-				// Move the extra stack onto the new bottom
+				// After disks are moved to extra, move bottom to goal
+				goal.push_back(start.back());
+				start.pop_back();
+				printAllTowers();
+				// Move the stack on extra onto the new bottom
 				moveDisks(thisDisk - 1, extra, goal, start);
 			}
 		}
 
 		void solve() {
-			printDisks();
+			cout << "\n";
+			printAllTowers();
 			moveDisks(totalDisks, tower1, tower3, tower2);
+			cout << "Solved in " << steps << " steps." << "\n\n";
 		}
 };
 
-int main() {
-	const int totalDisks = 3;
-	HanoiTowers towers = 3;
-	towers.solve();
-	system("pause");
+int getIntInput() {
+	string s;
+	while (true) {
+		getline(cin, s);
+		try { return stoi(s); }
+		catch (...) {}
+		cout << "Invalid input! Enter a positive integer: ";
+	}
 }
 
+int main() {
+	cout << "Towers of Hanoi solver by Aaron Jimenez." << "\n";
+	cout << "Enter \"0\" at any time to exit." << "\n";
+	cout << "-- -- -- -- -- -- -- -- -- -- -- --" << "\n";
+
+	while (true) {
+		cout << "Enter number of disks: ";
+		const int totalDisks = getIntInput();
+		if (totalDisks == 0) break;
+		HanoiTowers towers = totalDisks;
+		towers.solve();
+	}
+}
